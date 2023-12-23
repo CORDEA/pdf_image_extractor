cc72afbf5771d9f6463548c81d25e3083f1d823e
Yoshihiro Tanaka <contact@cordea.jp>
2023-12-23 21:23:57 +0900

    check if it's a pdf

--- lib/src/pdf_image_extractor_base.dart
+++ lib/src/pdf_image_extractor_base.dart
@@ -1,6 +1,22 @@
-// TODO: Put public facing types in this file.
+import 'dart:io';
+import 'dart:typed_data';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:collection/collection.dart';
+
+class PdfImageExtractor {
+  PdfImageExtractor(this.file);
+
+  final File file;
+  late Uint8List _bytes;
+
+  Future<List<dynamic>> extract() async {
+    _bytes = await file.readAsBytes();
+    if (_isPdf()) {
+      return [];
+    }
+    return [];
+  }
+
+  bool _isPdf() =>
+      IterableEquality().equals(_bytes.take(4), [0x25, 0x55, 0x44, 0x46]);
 }
