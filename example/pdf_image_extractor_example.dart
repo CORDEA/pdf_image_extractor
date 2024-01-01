9e7839b2747a1e3b3dd73dabfed30c7fb171e6de
Yoshihiro Tanaka <contact@cordea.jp>
2024-01-01 14:58:01 +0900

    add an example

--- example/pdf_image_extractor_example.dart
+++ example/pdf_image_extractor_example.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+import 'package:image/image.dart';
 import 'package:pdf_image_extractor/pdf_image_extractor.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main(List<String> arguments) async {
+  final file = File(arguments.first);
+  final rawImages = await PdfImageExtractor(file).extract();
+  final images = PdfImageProcessor(rawImages).write();
+  final date = DateTime.now();
+  images.forEachIndexed((i, e) async {
+    await encodePngFile('${date.millisecondsSinceEpoch}_$i.png', e);
+  });
 }
